2c58b42541401456c63e59fe2eef1cdebd268cb2
Vitor Garcia <vitfgarcia@gmail.com>
2018-07-26 09:54:10 -0300

    changed mocha ui to be tdd like

--- test/suite/google/google.test.ts
+++ test/suite/google/google.test.ts
@@ -1,14 +1,14 @@
 import { expect } from 'chai';
 import { rest } from '../../lib/helpers/rest';
 
-describe('Google API', () => {
-  it('Google Search Page Request Success', async () => {
+suite('Google API', () => {
+  test('Google Search Page Request Success', async () => {
     const response = await rest.get('https://www.google.com');
-    expect(response.status).to.be.eq(200, `A página de busca retornou o status ${response.status}`);
+    expect(response.status).to.be.eq(200, `The search page returned an error with http status = '${response.status}'`);
   });
 
-  it('Google Search Page Request Fail', async () => {
+  test('Google Search Page Request Fail', async () => {
     const response = { status: 400 };
-    expect(response.status).to.be.eq(200, `A página de busca retornou erro de status ${response.status}`);
+    expect(response.status).to.be.eq(200, `The search page returned an error with http status = '${response.status}'`);
   });
 });
